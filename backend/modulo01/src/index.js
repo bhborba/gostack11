916664030184143fd372058d8a434d38e5107aa3
bhborba <brunohenriqueborba@gmail.com>
2020-04-10 10:37:28 -0300

    M01: Tipos de Parâmetros

--- backend/modulo01/src/index.js
+++ backend/modulo01/src/index.js
@@ -2,15 +2,28 @@ const express = require('express');
 
 const app = express();
 
+app.use(express.json());
+
 app.get('/projects',(req,res) => {
+    const {title, owner} = req.query;
+
     return res.json(['Projeto 1', 'Projeto 2']);
 });
 
 app.post('/projects', (req,res) => {
+    const {title, owner} = req.body;
+
+    console.log(title);
+    console.log(owner);
+
     return res.json(['Projeto 1', 'Projeto 2', 'Projeto 3']);
 });
 
 app.put('/projects/:id', (req,res) => {
+    const params = req.params;
+
+    console.log(params);
+
     return res.json(['Projeto 4', 'Projeto 2', 'Projeto 3']);
 });
 
