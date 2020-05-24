abbd029fa6a27bee939b2d938f4a48c4cce41869
Bruno Henrique de Borba <brunohenriqueborba@gmail.com>
2020-05-24 17:36:43 -0300

    M01: Tipando objetos e vetores

--- typescript/src/routes.ts
+++ typescript/src/routes.ts
@@ -1,5 +1,14 @@
 import { Request, Response } from 'express';
+import createUser from './services/CreateUser';
 
 export function helloWorld(req: Request, res: Response) {
+    const user = createUser({
+        email: 'bruno@gmail.com',
+        password: '123456',
+        techs: ['Node', 'React', 'ReactNative',
+        { title: 'Javascript', experience: 100 },
+    ],
+    });
+
     return res.json({ message: 'Hello World'});
 }
