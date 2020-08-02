c27358cc283c1d3079f7b99e1ed66947acce4dba
Bruno Henrique de Borba <brunohenriqueborba@gmail.com>
2020-08-01 23:03:35 -0300

    M02: Rotas autenticadas

---
+++ backend/gobarber/src/config/auth.ts
@@ -0,0 +1,6 @@
+export default {
+  jwt: {
+    secret: '639196e9c031102815c6489db039fbb3',
+    expiresIn: '1d',
+  },
+};
--- backend/gobarber/src/services/CreateSessionService.ts
+++ backend/gobarber/src/services/CreateSessionService.ts
@@ -1,6 +1,7 @@
 import { getRepository } from 'typeorm';
 import { compare } from 'bcryptjs';
 import { sign } from 'jsonwebtoken';
+import authConfig from '../config/auth';
 
 import User from '../models/User';
 
@@ -30,9 +31,11 @@ class CreateSessionService {
       throw new Error('Invalid email or password');
     }
 
-    const token = sign({}, '639196e9c031102815c6489db039fbb3', {
+    const { secret, expiresIn } = authConfig.jwt;
+
+    const token = sign({}, secret, {
       subject: user.id,
-      expiresIn: '1d',
+      expiresIn,
     });
 
     return {
